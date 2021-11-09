68568c79de1628817f79af7fbb07b6cec6dcb3c1
StephenChips <tcw6991@gmail.com>
2021-11-09 18:43:52 +0800

    change gitignore

--- test/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Projects/cppjson/test/_deps/googletest-src
-# Build directory: /mnt/d/Projects/cppjson/test/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
